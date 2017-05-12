4bb71e8557ee2e28d34a3b4e28fe7d19fb8eefd4
Zachary Schafer <schafezp@gmail.com>
2017-05-11 21:25:34 -0400

    comment out text blob

--- apiator-cli/apiator-cli.go
+++ apiator-cli/apiator-cli.go
@@ -457,19 +457,19 @@ func main() {
 	}
 	app.EnableBashCompletion = true
 	
-	''' Test Post
-	jsonS := fmt.Sprintf(`{"id":"%s", "token":"%s", "document": %s, "doc_id": "%s"}`, "epid", "connection.auth.token", "{}", "docid")
-	resp, err := postJSON(`http://httpbin.org/post`, jsonS)	
-	if (err != nil) {
-		fmt.Println(err)
-	} else {
-		fmt.Println("RESPONSE: ")
-		bbytes, e := ioutil.ReadAll(resp.Body)
-		if (e != nil) {panic(e)}
-		fmt.Println(string(bbytes))
-		fmt.Println(resp)
-	}
-	'''
+	// ''' Test Post
+	// jsonS := fmt.Sprintf(`{"id":"%s", "token":"%s", "document": %s, "doc_id": "%s"}`, "epid", "connection.auth.token", "{}", "docid")
+	// resp, err := postJSON(`http://httpbin.org/post`, jsonS)	
+	// if (err != nil) {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println("RESPONSE: ")
+	// 	bbytes, e := ioutil.ReadAll(resp.Body)
+	// 	if (e != nil) {panic(e)}
+	// 	fmt.Println(string(bbytes))
+	// 	fmt.Println(resp)
+	// }
+	// '''
 
 	app.Run(os.Args)
 }
