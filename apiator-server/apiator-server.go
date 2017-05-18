c7a10b23ee505e7ccfa212e7664ab33bb08c1cc3
Zachary Schafer <schafezp@gmail.com>
2017-05-18 14:17:16 -0400

    Move sync related code into sync package

--- apiator-server/apiator-server.go
+++ apiator-server/apiator-server.go
@@ -17,81 +17,22 @@ import (
 import "./dbsolr"
 import "./config"
 import "./endpoint"
+import "./sync"
 // func handleCouchbaseError()
 var (
 	jwtSecret           = []byte("KHOzH8DJRHIPfC9Mq8yH")
 	conf = config.GetConfig()
-	operationsToApply = make([]QueuedOperation,5)
+	//reasonable initial length? currently set to 5
+	operationsToApply = make([]sync.QueuedOperation,5)
 )
 
+//TODO: consider best way to move this into sync
 func redisOperationFail(operation string){
 	fmt.Printf("Redis operation failed: %s \n",operation)
-	operationsToApply = append(operationsToApply,QueuedOperation{DbType:1,Operation:operation})
+	operationsToApply = append(operationsToApply,sync.QueuedOperation{DbType:1,Operation:operation})
 	// fmt.Printf("Redis operation failed: %s \n",operation)
 }
 
-//calling this function with go routine will sync the dbs
-// func syncdbs(syncRedis,syncSolr, quit chan int){
-func syncdbs(){
-	for _, op := range operationsToApply{
-		switch  op.DbType {
-		case 0://couchbase
-			fmt.Println("Don't handle couchbase queued ops'")
-			// switch op.functtype:
-			//do stuff depending on functtype passing in necessary args
-
-		case 1://redis
-			client := redis.NewClient(&redis.Options{
-				Addr:     conf.RedisServerAddr,
-				Password: conf.RedisServerPassword,
-				DB:       0, // use default DB
-			})
-			fmt.Println("Run failed redis command: %s",op.Operation)
-			fmt.Println(op.Operation)
-			err := client.Eval(op.Operation,[]string{})
-			//TODO: putting back here is dangerous if too many "bad" redis commands stack up
-			if err == nil{//if successful then remove that slice part
-				operationsToApply = append(operationsToApply[:0], operationsToApply[1:]...)
-			}
-			
-		case 2://solr
-			fmt.Println("Don't handle solr queued ops'")
-			
-		default:
-		}
-	}
-}
-
-func logStoredOps(){
-	for _,op := range operationsToApply{
-		switch  op.DbType {
-		case 0://couchbase
-			fmt.Println("Don't handle couchbase queued ops'")
-		case 1://redis
-			fmt.Println("Run failed redis command: %s",op.Operation)
-		case 2://solr
-			fmt.Println("Don't handle solr queued ops'")
-		default:
-		}
-	}
-}
-
-//QueuedRedisOperation represents a redis operation to be performed
-type QueuedRedisOperation struct {
-	
-}
-//When SOLR or Redis is down and we receive a request we should store it rather than ignoring it
-//to store it we will use a channel of Queued Operations
-
-//QueuedOperation represents a queued instruction to preform 
-type QueuedOperation struct {
-	//TODO: make Databasetype be an enum type rather than int
-	//currently 0 -> couchbase
-	//          1 -> redis
-	//          2 -> solr
-	DbType int `form:"dbtype" json:"dbtype" binding:"required"`
-	Operation string `form:"operation" json:"operation" binding:"required"`
-}
 
 type DataCRUD struct {
 	ID    string      `json:"id" binding:"required"`
@@ -257,8 +198,6 @@ func main() {
 	var bucketerror error
 	var geterror error
 	var connecterror error
-	//start sync dbs asap
-	go syncdbs()
 	cluster, connecterror = gocb.Connect(conf.CouchbaseServerAddr)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -297,7 +236,7 @@ func main() {
 		})
 	})
 	r.GET("log-stored-ops", func(c *gin.Context) {
-		logStoredOps()
+		sync.LogStoredOps(operationsToApply)
 		
 	})
 	r.GET("/redis/reset-user-token/:username", func(c *gin.Context) {
@@ -355,7 +294,7 @@ func main() {
 	
 	//issues attempt to sync redis
 	r.GET("/redis/sync", func(c *gin.Context) {
-		syncdbs()
+		sync.Syncdbs(operationsToApply,conf)
 		c.JSON(200, gin.H{
 			"solr-message": "Start attempt to sync manually",
 		})
